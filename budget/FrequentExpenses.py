5f8cfb20de13fe08e05ccbe7740166b18202316e
kennydb <kbrodrick61@gmail.com>
2021-09-25 15:59:34 -0700

    Completed module

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,36 @@
+import Expense
+import collections
+import matplotlib.pyplot as plt
+
+expenses = Expense.Expenses()
+
+# Read in the Spending Data
+expenses.read_expenses('..\\data\\spending_data.csv')
+
+# Create a LIst of the Spending Categories
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+# Count Categories with a Counter Collection
+spending_counter = collections.Counter(spending_categories)
+
+# Get the Top 5 Categories
+top5 = spending_counter.most_common(5)
+
+
+# Convert the Dictionary to 2 Lists
+categories, count = zip(*top5)
+
+# Plot the Top 5 Most Common Categories
+fig, ax = plt.subplots()
+
+# Create the bar chart
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
+
+print(spending_counter)
+print(top5)
+print(categories, count)
