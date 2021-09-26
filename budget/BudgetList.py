9dc5384ea7e7e5d637da18acc1f6e839f221e854
kennydb <kbrodrick61@gmail.com>
2021-09-25 20:51:35 -0700

    There was an issue testing your project. Try to run your tests locally and confirm there aren't any issues.

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,10 +1,10 @@
-from . import Expense
+from budget import Expense
 
 class BudgetList():
 
     def __init__(self, budget):
         self.budget = budget
-        self.sum_expenses =0
+        self.sum_expenses = 0
         self.expenses = []
         self.sum_overages = 0
         self.overages = []
@@ -25,13 +25,11 @@ class BudgetList():
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
+    expenses.read_expenses('..//data//spending_data.csv')
 
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-
-
-print(str('The count of all expenses: ' + str(len().myBudgetList)))
+    print(str('The count of all expenses: ' + str(len(myBudgetList))))
 
 
 if __name__ == "__main__":
