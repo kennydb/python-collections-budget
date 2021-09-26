cfba1a300a0e6ad47fb646ff8976d08a42c86487
kennydb <kbrodrick61@gmail.com>
2021-09-26 13:34:17 -0700

    updated ExpensesCategories and Expense

--- budget/Expense.py
+++ budget/Expense.py
@@ -46,16 +46,15 @@ class Expenses():
 
 
     def categorize_set_comprehension(self):
-        necessary_expenses = {}
-        for x in self.list:
-            if (x.category == 'Phone' or x.category == 'Auto and Gas' or
-                x.category == 'Classes' or x.category== 'Utilities' or
+        necessary_expenses = {x for x in self.list}
+        if (x.category == 'Phone' or x.category == 'Auto and Gas' or
+                x.category == 'Classes' or x.category == 'Utilities' or
                 x.category == 'Mortgage'):
-                food_expenses = x.category == 'Groceries' or x.category == 'Eating Out'
+            food_expenses = category == 'Groceries' or category == 'Eating Out'}
             else:
-                unnecessary_expenses = set(self.list)
-                set.remove(necessary_expenses, food_expenses)
-                return[necessary_expenses, food_expenses, unnecessary_expenses]
+            unnecessary_expenses = set(self.list)
+            set.remove(necessary_expenses, food_expenses)
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
 
     divided_set_comp = expenses.categorize_set_comprehension()
 
--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,5 @@
 from . import Expense
-import matplotlib.pyplot as plt
+import timeit
 
 
 def main():
@@ -7,6 +7,11 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = categorize_set_comprehension()
+
+    print(timeit.timeit(stmt="pass", setup='''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')''', number=100000, globals=globals))
 
 
 if __name__ == "__main__":
