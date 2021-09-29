97b058033991a4462fb0486dd5b743d790d90018
kennydb <kbrodrick61@gmail.com>
2021-09-29 13:40:45 -0700

    module in WIP

--- budget/Expense.py
+++ budget/Expense.py
@@ -57,8 +57,8 @@ class Expenses():
 
     divided_set_comp = categorize_set_comprehension()
 
-    if not divided_set_comp == divided_for_loop:
-        print('Sets are NOT equal by == test')
-        for a,b in zip(divided_for_loop, divided_set_comp):
-            if not set.issubset(a, b):
-                print("Sets are NOT equal by subset test")
+    # if not divided_set_comp == divided_for_loop:
+    #     print('Sets are NOT equal by == test')
+    #     for a,b in zip(divided_for_loop, divided_set_comp):
+    #         if not set.issubset(a, b):
+    #             print("Sets are NOT equal by subset test")
