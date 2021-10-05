ba231f880daa2c09e25dd14bb8efc5ab401ae80e
kennydb <kbrodrick61@gmail.com>
2021-10-04 18:48:54 -0700

    updated module

--- ExpenseCategories.py
+++ ExpenseCategories.py
@@ -1,4 +1,5 @@
 from budget import Expense
+import matplotlib.pyplot as plt
 import timeit
 
 
@@ -7,18 +8,15 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
-    print(expenses.divided_for_loop)
-    print(expenses.divided_set_comp)
-    print(timeit.timeit(stmt="pass", setup='''from . import Expense
-    expenses = Expense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')''', number=100000, globals=globals))
+    # print(timeit.timeit(stmt="pass", setup='''from . import Expense)
 
-    if not divided_set_comp == divided_for_loop():
+    if not divided_set_comp == divided_for_loop:
         print('Sets are NOT equal by == test')
 
     for a, b in zip(divided_for_loop, divided_set_comp):
-        if not (a.Issubset(b) and b.Issubset(a)):
+        if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
+
 
 if __name__ == "__main__":
     main()
