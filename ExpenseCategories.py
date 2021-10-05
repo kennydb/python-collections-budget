6a975860b6a2194d8e1dec1fbc29dd26e23bf8e5
kennydb <kbrodrick61@gmail.com>
2021-10-04 19:04:40 -0700

    updated module

--- ExpenseCategories.py
+++ ExpenseCategories.py
@@ -8,7 +8,6 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
-    # print(timeit.timeit(stmt="pass", setup='''from . import Expense)
 
     if not divided_set_comp == divided_for_loop:
         print('Sets are NOT equal by == test')
